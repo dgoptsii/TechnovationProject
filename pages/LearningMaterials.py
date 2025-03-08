8d808e41af924ae7bdffbe6e754d459319a7fb12
Kira2812 <mironovakira121@gmail.com>
2025-03-08 18:11:21 +0200

    fix page partners

--- pages/LearningMaterials.py
+++ pages/LearningMaterials.py
@@ -2,37 +2,31 @@ import streamlit as st
 import utils
 # Function to load the CSS
 
+
+Timecode =("Таймкод: 1.24- алфавіт. У цьому відео детально розповідають що таке дактиль, базові жести і дають пораду як його вивчити")
+Video = ("Відео, яке допоможе вивчити привітання")
+
 def app():
- # Load the CSS file to apply styles globally
- utils.load_css("style.css")
-
- st.header("Навчальні матеріали")
- image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp4si91mL3kimJO2EZY-_sB6Gt5bE4oktPdw&s"
- st.image(image_url, caption="А", use_column_width=True)
-
- st.header("Навчальні матеріали")
- st.markdown(
-     f"""
+  st.header("Навчальні матеріали")
+  st.markdown(
+    f"""
     <div style="text-align:center;">
-        <imge_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp4si91mL3kimJO2EZY-_sB6Gt5bE4oktPdw&s" width="300" height="200">
-        <p>А</p>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTp4si91mL3kimJO2EZY-_sB6Gt5bE4oktPdw&s" width="300" height="500">
     </div>
     """,
     unsafe_allow_html=True
-    )
- 
+)
+
  # Videos 
-st.header ("Відео жестів")
-
-
-col1, col2 = st.columns(2)  # Два столбца
-
-with col1:
+  st.markdown ("Відео жестів")
+  col1, col2 = st.columns(2)  # Два столбца
+  with col1:
     st.video("https://youtu.be/J6Kb4hMAbHE?si=fjXLA9rZvZ9WBX_Z")
-    st.markdown("Таймкод: 1.24- алфавіт. У цьому відео детальніше розповідають що таке дактиль, базові жести і дають пораду як його вивчити")
+    st.markdown(f'<div class="text">{Timecode}</div>', unsafe_allow_html=True) 
+    with col2:
+      st.video("https://youtu.be/YKD8q4OQhPo?si=4YgiNlWU8sDv-dKP") 
+      st.markdown(f'<div class="text">{Video}</div>', unsafe_allow_html=True) 
 
-
-with col2:
-    st.video("https://youtu.be/YKD8q4OQhPo?si=4YgiNlWU8sDv-dKP") 
-
-    st.markdown("Відео, яке допоможе вивчити привітання")
+  st.header(" Ресурси для вивчення жестової мови")
+  st.markdown("[Spread The Sign – онлайн-словник](https://spreadthesign.com/uk.ua/search/?cls=1)")
+  st.markdown("[Курс жестової мови на Megogo](https://megogo.net/ua/view/3820211-kurs-zhestovo-movi-ukra-nskoyu-movoyu.html)")
