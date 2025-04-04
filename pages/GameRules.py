8bbd6ad7bcb091cb742e5559a9902397b06f7d95
AnnaSlysh <anna.slysh09@gmail.com>
2025-04-04 21:29:04 +0300

    ///

--- pages/GameRules.py
+++ pages/GameRules.py
@@ -3,23 +3,9 @@ import utils
 
 # Function to load the CSS
 
-rules = (
-    "Гра складається зі статичних жестів української мови. По черзі ви показуєте літери з перервами. Складність на початку вибираєте.<br>"
-    "Для того щоб гра зарахувала вашу відповідь, тримайте руку в одному положенні 5 секунд.<br>"
-    "Відсутні такі букви як: Ґ Д З Ї Й К Ц Щ Ь, оскільки вони нестатичні.<br>"
-    "За кожну неправильну відповідь квітка втрачає одну пелюстку. Якщо квітка залишилась без пелюсток, гра програна.<br>"
-    "Коли слово вгадано відображається вгадане слово і привітання переможця."
-)
-
-def app():
-    # Load the CSS file to apply styles globally
-    utils.load_css("style.css")
-    st.markdown('<div class="title">Правила гри</div>', unsafe_allow_html=True)
-    st.markdown(f'<div class="text">{rules}</div>', unsafe_allow_html=True)
-
-def app():
-  st.header("Приклади")
-  st.markdown(
+rules1 = (
+    "Гра складається зі статичних жестів української мови. По черзі ви показуєте літери з перервами. Складність на початку вибираєте.<br>")
+st.markdown(
     f"""
     <div style="text-align:center;">
         <img src="https://i.postimg.cc/jSM8KWcd/1.jpg">
@@ -27,3 +13,19 @@ def app():
     """,
     unsafe_allow_html=True
 )
+rules2 = ("Для того щоб гра зарахувала вашу відповідь, тримайте руку в одному положенні 5 секунд.<br>")
+rules3 = ("Відсутні такі букви як: Ґ Д З Ї Й К Ц Щ Ь, оскільки вони нестатичні.<br>")
+rules4 = ("За кожну неправильну відповідь квітка втрачає одну пелюстку. Якщо квітка залишилась без пелюсток, гра програна.<br>")
+rules5 =("Коли слово вгадано відображається вгадане слово і привітання переможця.")
+
+def app():
+    # Load the CSS file to apply styles globally
+    utils.load_css("style.css")
+    st.markdown('<div class="title">Правила гри</div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="text">{rules1}</div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="text">{rules2}</div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="text">{rules3}</div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="text">{rules4}</div>', unsafe_allow_html=True)
+    st.markdown(f'<div class="text">{rules5}</div>', unsafe_allow_html=True)
+
+  
