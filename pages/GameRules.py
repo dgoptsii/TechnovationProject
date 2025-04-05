97cef7004335d9c6d080630984241b9618ebe186
AnnaSlysh <anna.slysh09@gmail.com>
2025-04-05 11:44:03 +0300

    ///

--- pages/GameRules.py
+++ pages/GameRules.py
@@ -21,11 +21,45 @@ def app():
         <img src="https://i.postimg.cc/mknsdQ2c/1.jpg">
     </div>
     """,
-    unsafe_allow_html=True
-)
+    unsafe_allow_html=True)
     st.markdown(f'<div class="text">{rules2}</div>', unsafe_allow_html=True)
     st.markdown(f'<div class="text">{rules3}</div>', unsafe_allow_html=True)
     st.markdown(f'<div class="text">{rules4}</div>', unsafe_allow_html=True)
+    st.markdown(
+    f"""
+    <div style="text-align:center;">
+        <img src="https://i.postimg.cc/fLJZY2M4/3.jpg">
+    </div>
+    """,
+    unsafe_allow_html=True)
+    st.markdown(
+    f"""
+    <div style="text-align:center;">
+        <img src="https://i.postimg.cc/sg4VzGTL/2.jpg">
+    </div>
+    """,
+    unsafe_allow_html=True)
     st.markdown(f'<div class="text">{rules5}</div>', unsafe_allow_html=True)
+    st.markdown(
+    f"""
+    <div style="text-align:center;">
+        <img src="https://i.postimg.cc/0jSsBz6c/4.jpg">
+    </div>
+    """,
+    unsafe_allow_html=True)
+    st.markdown(
+    f"""
+    <div style="text-align:center;">
+        <img src="https://i.postimg.cc/NG2BMzNm/5.jpg">
+    </div>
+    """,
+    unsafe_allow_html=True)
+    st.markdown(
+    f"""
+    <div style="text-align:center;">
+        <img src="https://i.postimg.cc/fT4Z0fFF/6.jpg">
+    </div>
+    """,
+    unsafe_allow_html=True)
 
   
