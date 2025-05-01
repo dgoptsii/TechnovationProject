9bd958f47a0de845a7e98db2599be3458a04a4ca
ShibaSmille <tt2036.2017@gmail.com>
2025-05-01 19:55:30 +0300

    Install changes

--- pages/Main.py
+++ pages/Main.py
@@ -6,6 +6,7 @@ import AboutUs
 import PublicOrganizations
 import LearningMaterials
 import utils
+from streamlit_navigation_bar import st_navbar
 
 # Сторінки
 PAGES = {
@@ -16,40 +17,17 @@ PAGES = {
     "Допомогти проекту": Help, 
     "Громадські організації": PublicOrganizations
 }
+st.set_page_config(layout="wide")
+
 
 # Кастомне меню з HTML
-st.markdown("""
-    <style>
-    .nav-container {
-        border-top: 3px solid #345753;  /* Верхняя граница */
-        border-bottom: 3px solid #345753;  /* Нижняя граница */
-        width: 1200px;  /* Ширина слайдбара (можно настроить) */
-        background-color: #3DB387;
-        padding: 10px;
-    }
-    .nav-container a {
-        margin-right: 30px;
-        color: white;
-        font-weight: bold;
-        text-decoration: none;
-    }
-    .nav-container a:hover {
-        text-decoration: underline;
-    }
-    </style>    
-    <div class="nav-container">
-        <a href="?page=Про проект">Про проект</a>
-        <a href="?page=Правила гри">Правила гри</a>
-        <a href="?page=Навчальні матеріали">Навчальні матеріали</a>
-        <a href="?page=Гра">Гра</a>
-        <a href="?page=Допомогти проекту">Допомогти проекту</a>
-        <a href="?page=Громадські організації">Громадські організації</a>
-    </div>
-""", unsafe_allow_html=True)
+page = st_navbar(["GameRules", "Game", "Help", "AboutUs", "PublicOrganizations", "LearningMaterials"])
+st.write(page)
 
 # Вибір сторінки
 query_params = st.query_params
 selection = query_params.get("page", ["Про проект"])[0]
+
 page = PAGES.get(selection, AboutUs)
 
 utils.load_css("style.css")
