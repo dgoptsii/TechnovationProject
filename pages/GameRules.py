359c2ebc7daa19bc7104fd7cdd070d0d1fc72e41
Kira2812 <mironovakira121@gmail.com>
2025-05-01 19:09:14 +0300

    GameRules.py rules are edited

--- pages/GameRules.py
+++ pages/GameRules.py
@@ -31,14 +31,14 @@ def app():
         'uk': {
             'title': "Правила гри",
             'rules': [
-                "Гра складається зі статичних жестів української мови. По черзі ви показуєте літери з перервами. Складність на початку вибираєте.<br>",
-                "На кожному рівні з’являтиметься вікно з камерою, у якому ви зможете виконувати жести за допомогою скелетної моделі руки.<br>",
+                "Спочатку обирається рівень складності. Гра побудована на жестах алфавіту української мови: літери демонструються по черзі з паузами.<br>",
+                "На кожному рівні з’являється вікно з камерою, в якому ви виконуватимете жести, а система розпізнаватиме їх за допомогою скелетної моделі руки.<br>",
                 "Всі жести в грі виконуються правою рукою.<br>",
                 "Ви можете бачити слово як серію пропусків («_»), що позначають кожну літеру. Це означає, що кожна літера, яку ви ще не вгадали, залишиться у слові у вигляді підкреслення (_).<br>"
                 "Для того щоб гра зарахувала вашу відповідь, тримайте руку в одному положенні 5 секунд.<br>"
                 "Якщо жест вже був показаний і розпізнаний, він буде позначений як «already captured».<br>",
                 "Відсутні такі букви як: Ґ Д З Ї Й К Ц Щ Ь, оскільки вони нестатичні.<br>",
-                "Після вгадування слова на екрані відображається вгадане слово та повідомлення з привітанням переможця.<br>"
+                "Після того як ви вгадали всі літери, слово відображається на екране  та повідомлення з привітанням переможця.<br>"
                 "За кожну неправильну відповідь квітка втрачає одну пелюстку. Якщо квітка залишилась без пелюсток, гра програна.<br>",
                 "Гра лише українською мовою."
             ]
@@ -46,12 +46,12 @@ def app():
         'en': {
             'title': "Game Rules",
             'rules': [
-                "The game consists of static gestures of the Ukrainian sign language. You sequentially show letters with pauses. Choose difficulty at the start.<br>",
-                "At each level, a window with a camera will appear, where you can perform gestures using a skeletal model of your hand.<br>",
+                "First, you choose the difficulty level. The game is based on the gestures of the Ukrainian alphabet: letters are shown in turn with pauses.<br>",
+                "At each level, a window with a camera appears in which you will perform gestures, and the system will recognize them using a skeletal model of the hand.<br>",
                 "All gestures in the game are performed with the right hand.<br>",
                 "You can see the word as a series of spaces (“_”) representing each letter. This means that any letter you haven't guessed yet will remain in the word as an underscore (_).<br>",
                 "To have your answer counted, hold your hand in the same position for 5 seconds.<br>",
-                "After guessing a word, the screen displays the guessed word and a message congratulating the winner.<br>",
+                "After you have guessed all the letters, the word is displayed on the screen and a message congratulating the winner.<br>",
                 "Letters like: Ґ, Д, З, Ї, Й, К, Ц, Щ, Ь are missing because they are not static.<br>",
                 "For each wrong answer, the flower loses a petal. If the flower loses all petals, the game is over.<br>",
                 "The game is only available in Ukrainian."
